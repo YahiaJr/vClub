cc78c431a367a2b427fe64c33050c252f432a3b2
Hesham Ramdan <55190339+hesham5avanty@users.noreply.github.com>
2020-06-01 21:47:20 +0200

    contact form & Person calss
    * Creating Contact form.
    * Creating the person class.

---
+++ models/person.php
@@ -0,0 +1,14 @@
+<?php
+
+    class person
+    {
+
+        public $fname;
+        public $lname;
+        public $birthdate;
+        public $email;
+        public $password;
+        public $sport;
+        public $phone;
+        public $salary;
+    }
