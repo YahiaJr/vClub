28a099e2ad195265fecf6b4ec56ffa2a0039b6d3
omarmohammad1998 <49873871+omarmohammad1998@users.noreply.github.com>
2020-06-01 20:53:09 +0200

    updating front
    * creating Login Form.
    * Creating the about page

---
+++ views/about.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>About - Club System Website</title>
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+</head>
+
+<body>
+<div class="header">
+    <div>
+        <a href="index.php" id="logo"><img src="images/logo.png" alt="logo"></a>
+        <ul>
+            <li>
+                <a href="index.php"><span>H</span>ome</a>
+            </li>
+            <li>
+                <a href="contact.php"><span>C</span>ontact</a>
+            </li>
+            <li class="selected">
+                <a href="about.php"><span>A</span>bout</a>
+            </li>
+            <li>
+                <a href="application.php"><span>R</span>egister</a>
+            </li>
+            <li>
+                <a href="staff.php"><span>L</span>ogin</a>
+            </li>
+
+        </ul>
+    </div>
+</div>
+<div class="body">
+    <div>
+        <div>
+            <div>
+                <div class="about">
+                    <div>
+                        <h2>ABOUT OUR CLUB</h2>
+                        <div>
+                            <h3><span>W</span>e <span>H</span>ave a <span>S</span>ystem <span>O</span>f <span>C</span>lub
+                            </h3>
+                            <p>
+                                A few clubs has a club system and web application for it .
+                                It is found that many people suffer from going to the club to ask about the timetable
+                                and reserve the activities in the club and also few people only know about the important
+                                news such that competitions and trips because of that we make a web application
+                                facilitates all of this .
+                            </p>
+                            <h3><span>C</span>lub <span>S</span>ystem</h3>
+                            <p>
+                                Our website talk about club system that recover all the activities in the club .
+                                The system does not specifically waste time , but the opposite it makes you able to know
+                                all the news quickly so that it does not waste you much time .
+                            </p>
+                        </div>
+                    </div>
+                    <div>
+                        <div>
+                            <img src="images/camping2.jpg" alt="">
+                            <h3><span>B</span>e <span>P</span>art of <span>O</span>ur <span>C</span>lub</h3>
+                            <p>
+                                We solve the problem of announcing the important news in the club such that competitions
+                                , parties and trips , we solve also the problem of paper writing and make a database to
+                                put all information about club in this , we also solve the distance problem for members
+                                to see the timetable and reserve in the activities.
+                            </p>
+                        </div>
+                        <div>
+                            <h3><span>C</span>lub details</h3>
+                            <p>
+                                News: <br/> The site makes it easy for you to watch all the latest news of the club
+                                without having to make an
+                                effort.<br/>
+                            </p>
+                            <hr>
+                            <p>
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum
+                                has been the industry's standard dummy text ever since the 1500s, when an unknown
+                                printer took a galley of type and scrambled it to make a type specimen book. It has
+                                survived not only five centuries, but also the leap into electronic typesetting,
+                                remaining essentially unchanged. It was popularised in the 1960s with the release of
+                                Letraset sheets containing.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php include './footer.php'; ?>
+</body>
+
+</html>
