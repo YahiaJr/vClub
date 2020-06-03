88e07e92f0127fd86953a94dbaadcaa43c880e18
Muhammad Yahia <37158766+YahiaJr@users.noreply.github.com>
2020-06-03 20:38:12 +0200

    Member view + footer update
    * created the game filtering functions (filter + remove filter).
    * Updated the footer to include admin new pages.

--- models/app_user.php
+++ models/app_user.php
@@ -1,13 +1,38 @@
 <?php
-
     include "connection.php";
     include_once "person.php";
-
-
+    
     class App_user extends person
     {
 
-        //Contact Form logic #can be used for any contact form in the future#
+        //Filtering_Time-Table
+        public function filter()
+        {
+            $sql = 'SELECT `game`, `day`, `time` FROM `time_table` WHERE game = "' . $_POST['filtertt'] . '"';
+            $result = mysqli_query($GLOBALS['conn'], $sql);
+            while ($row = mysqli_fetch_array($result)) {
+                echo "<tr class=\"filterr\">";
+                echo "<td class=\"filterr\">" . $row['game'] . "</td>";
+                echo "<td class=\"filterr\">" . $row['day'] . "</td>";
+                echo "<td class=\"filterr\">" . $row['time'] . "</td>";
+                echo "</tr>";
+            }
+        }
+
+        public function no_Filter()
+        {
+            $sql = 'SELECT `game`, `day`, `time` FROM `time_table`';
+            $result = mysqli_query($GLOBALS['conn'], $sql);
+            while ($row = mysqli_fetch_array($result)) {
+                echo "<tr class=\"filterr\">";
+                echo "<td class=\"filterr\">" . $row['game'] . "</td>";
+                echo "<td class=\"filterr\">" . $row['day'] . "</td>";
+                echo "<td class=\"filterr\">" . $row['time'] . "</td>";
+                echo "</tr>";
+            }
+        }
+
+        //Contact Form logic #can be used for any contact#
         public function Add_message($is_member, $c_name, $c_email, $c_message)
         {
             $contact = "INSERT INTO `contact`(`is_member`,`name`, `email`, `message`) 
--- views/footer.php
+++ views/footer.php
@@ -18,21 +18,29 @@
             <a href="http://facebook.com" class="facebook">Facebook</a> <a href="http://twitter.com" class="twitter">Twitter</a>
             <a href="http://google.com" class="google">Google+</a>
         </div>
+
     </div>
-    
     <div class="flogo">
+
         <?php //Go to Home Page From footer logo
-                switch (basename($_SERVER['PHP_SELF'])) {
-                    //member pages
-                    case "index_members.php":
-                    case "contact_member.php":
-                    case "search_member.php":
-                        echo '<a href="index_members.php" id="logo"><img style="margin-top: -60px" src="images/logo.png" alt="logo"></a>';
-                        break;
-                    //default
-                    default:
-                        echo '<a href="index.php" id="logo"><img style="margin-top: -60px" src="images/logo.png" alt="logo"></a>';
-                }
-            ?>
+            switch (basename($_SERVER['PHP_SELF'])) {
+                //member pages
+                case "index_members.php":
+                case "contact_member.php":
+                case "search_member.php":
+                    echo '<a href="index_members.php" id="logo"><img style="margin-top: -60px" src="images/logo.png" alt="logo"></a>';
+                    break;
+                //admin_pages
+                case "index_admin.php":
+                case "addnewadmin.php":
+                case "addnewmember.php":
+                case "search_admin.php":
+                    echo '<a href="index_admin.php" id="logo"><img style="margin-top: -60px" src="images/logo.png" alt="logo"></a>';
+                    break;
+                //default
+                default:
+                    echo '<a href="index.php" id="logo"><img style="margin-top: -60px" src="images/logo.png" alt="logo"></a>';
+            }
+        ?>
     </div>
 </div>
