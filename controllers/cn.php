210fe2432c87ab169b0de0608a1ae6f4720cfb04
emad20140255 <emad.ahmed20140255@gmail.com>
2020-06-04 12:17:42 +0200

    creating other admins (c+m)
     created (add_admin) function for admin module.
    * updated controller with logic for adding admin accounts

--- controllers/cn.php
+++ controllers/cn.php
@@ -45,7 +45,7 @@
                     }
                 }
         }
-
+//Visitor_Logic
         //Add_Contact_Messages##
         if ($_REQUEST['do'] == 'contact') {
             $c_name = $_POST['c_name'];
@@ -67,29 +67,45 @@
             $op->filter();
         }
 
-        //Add a member to database
-        if ($_GET['do'] == 'login') {
-            $fname = $_POST['fname'];
-            $lname = $_POST['lname'];
-            $email = $_POST['email'];
-            $password = $_POST['password'];
-            $birthdate = $_POST['birthdate'];
-            $phone = $_POST['phone'];
-            //$m_sport = $_POST['m_sport'];
-            switch ($_POST['m_sport']) {
-                case 'Swimming':
-                    $m_sport = "Swimming";
-                    break;
-                case 'Football':
-                    $m_sport = "Football";
-                    break;
-                case 'Fitness':
-                    $m_sport = "Fitness";
-                    break;
+//Admin_Logic
+        //Adding New Users
+            ##Add_NEW_MEMBERS## // Same Logic used by (admin and visitor member application)
+            if ($_GET['do'] == 'login') {
+                $fname = $_POST['fname'];
+                $lname = $_POST['lname'];
+                $email = $_POST['email'];
+                $password = $_POST['password'];
+                $birthdate = $_POST['birthdate'];
+                $phone = $_POST['phone'];
+                //$m_sport = $_POST['m_sport'];
+                switch ($_POST['m_sport']) {
+                    case 'Swimming':
+                        $m_sport = "Swimming";
+                        break;
+                    case 'Football':
+                        $m_sport = "Football";
+                        break;
+                    case 'Fitness':
+                        $m_sport = "Fitness";
+                        break;
+                }
+    
+                $op = new admin();
+                $op->Add_member($fname, $lname, $email, $password, $birthdate, $phone, $m_sport);
             }
-            $op = new admin();
-            $op->Add_member($fname, $lname, $email, $password, $birthdate, $phone, $m_sport);
-        }
+    
+            ##ADD_NEW_ADMINS##
+            if ($_GET['do'] == 'add_new_admin') {
+                $fname = $_POST['ad_fname'];
+                $lname = $_POST['ad_lname'];
+                $phone = $_POST['ad_phone'];
+                $salary = $_POST['ad_salary'];
+                $email = $_POST['ad_email'];
+                $pass = $_POST['ad_password'];
+    
+                $op = new admin();
+                $op->Add_admin($fname, $lname, $phone, $salary, $email, $pass);
+            }    
     }
 
 
--- models/admin.php
+++ models/admin.php
@@ -33,5 +33,13 @@
         VALUES ('$mfname', '$mlname','$m_email', '$m_password', '$m_birthdate' , '$m_phone' , '$m_sport')";
             $val = mysqli_query($GLOBALS['conn'], $add_query);
         }
+
+        //creating new admins
+        public function Add_admin($fname, $lname, $phone, $salary, $email, $password)
+        {
+            $add_query = "INSERT INTO `admin`(`ad_fname`, `ad_lname`, `ad_phone`, `ad_salary`, `ad_email`, `ad_password`)
+      VALUES ('$fname' , '$lname' , '$phone' , '$salary' , '$email' , '$password')";
+            $val = mysqli_query($GLOBALS['conn'], $add_query);
+        }
     }
     ?>
